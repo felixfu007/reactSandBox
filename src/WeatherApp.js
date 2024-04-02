ca2e81108e36b3bba72ab961a48c7ba81cbb2328
阿符 <57716497+felixfu007@users.noreply.github.com>
2024-04-02 06:51:40 +0000

    feat. add css tool emotion

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+// STEP 1：載入 emotion 的 styled 套件
+import styled from "@emotion/styled";
+
+// STEP 2：定義帶有 styled 的 component
+const Container = styled.div`
+  background-color: #ededed;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WeatherCard = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: 0 1px 3px 0 #999999;
+  background-color: #f9f9f9;
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
+
+// STEP 3：把上面定義好的 styled-component 當成元件使用
 const WeatherApp = () => {
-  return <h1>Weather</h1>;
+  return (
+    <Container>
+      <WeatherCard>
+        <h1>Weather</h1>
+      </WeatherCard>
+    </Container>
+  );
 };
 
 export default WeatherApp;
