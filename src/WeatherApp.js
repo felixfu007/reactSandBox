adb2f68cce079ea15e5e2ac4b48786ea4e6cdf9b
阿符 <57716497+felixfu007@users.noreply.github.com>
2024-04-02 07:28:26 +0000

    feat. weather img and css

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,9 +1,11 @@
 import React from "react";
-
-// STEP 1：載入 emotion 的 styled 套件
 import styled from "@emotion/styled";
 
-// STEP 2：定義帶有 styled 的 component
+import { ReactComponent as CloudyIcon } from "./images/day-cloudy.svg";
+import { ReactComponent as AirFlowIcon } from "./images/airFlow.svg";
+import { ReactComponent as RainIcon } from "./images/rain.svg";
+import { ReactComponent as RedoIcon } from "./images/refresh.svg";
+
 const Container = styled.div`
   background-color: #ededed;
   height: 100%;
@@ -21,12 +23,105 @@ const WeatherCard = styled.div`
   padding: 30px 15px;
 `;
 
-// STEP 3：把上面定義好的 styled-component 當成元件使用
+// 透過 props 取得傳進來的資料
+// props 會是 {theme: "dark", children: "台北市"}
+// 透過傳進來的資料決定要呈現的樣式
+const Location = styled.div`
+  font-size: 28px;
+  color: ${(props) => (props.theme === "dark" ? "#dadada" : "#212121")};
+  margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+  font-size: 16px;
+  color: #828282;
+  margin-bottom: 30px;
+`;
+
+const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Temperature = styled.div`
+  color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  margin-bottom: 20px;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Cloudy = styled(CloudyIcon)`
+  /* 在這裡寫入 CSS 樣式 */
+  flex-basis: 30%;
+`;
+
+const Redo = styled(RedoIcon)`
+  /* 在這裡寫入 CSS 樣式 */
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  cursor: pointer;
+`;
+
 const WeatherApp = () => {
   return (
     <Container>
       <WeatherCard>
-        <h1>Weather</h1>
+        <Location theme="dark">台北市</Location>
+        <Description>多雲時晴</Description>
+        <CurrentWeather>
+          <Temperature>
+            23 <Celsius>°C</Celsius>
+          </Temperature>
+          <Cloudy />
+        </CurrentWeather>
+        <AirFlow>
+          <AirFlowIcon />
+          23 m/h
+        </AirFlow>
+        <Rain>
+          <RainIcon />
+          48%
+        </Rain>
+        <Redo />
       </WeatherCard>
     </Container>
   );
