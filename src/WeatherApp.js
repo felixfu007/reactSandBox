0ea77389ae9144543dfd1616dceb58a63ab0b4dd
阿符 <57716497+felixfu007@users.noreply.github.com>
2024-04-08 06:36:05 +0000

    feat. 重構

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -3,15 +3,18 @@ import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { ThemeProvider } from "@emotion/react";
 //載入emotion的styled css工具
 import styled from "@emotion/styled";
-import WeatherIcon from "./WeatherIcon.js";
+import WeatherCard from "./WeatherCard";
+
 import sunriseAndSunsetData from "./sunrise-sunset.json";
 
-//載入SVG圖片
-import { ReactComponent as CloudyIcon } from "./images/day-cloudy.svg";
-import { ReactComponent as AirFlowIcon } from "./images/airFlow.svg";
-import { ReactComponent as RainIcon } from "./images/rain.svg";
-import { ReactComponent as RefreshIcon } from "./images/refresh.svg";
-import { ReactComponent as LoadingIcon } from "./images/loading.svg";
+const Container = styled.div`
+  /* 在 Styled Component 中可以透過 Props 取得對的顏色 */
+  background-color: ${({ theme }) => theme.backgroundColor};
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const theme = {
   light: {
@@ -33,117 +36,6 @@ const theme = {
   },
 };
 
-const Container = styled.div`
-  /* 在 Styled Component 中可以透過 Props 取得對的顏色 */
-  background-color: ${({ theme }) => theme.backgroundColor};
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const WeatherCard = styled.div`
-  position: relative;
-  min-width: 360px;
-  box-shadow: ${({ theme }) => theme.boxShadow};
-  background-color: ${({ theme }) => theme.foregroundColor};
-  box-sizing: border-box;
-  padding: 30px 15px;
-`;
-
-// 透過 props 取得傳進來的資料
-// props 會是 {theme: "dark", children: "台北市"}
-// 透過傳進來的資料決定要呈現的樣式
-const Location = styled.div`
-  font-size: 28px;
-  color: ${({ theme }) => theme.titleColor};
-  margin-bottom: 20px;
-`;
-
-const Description = styled.div`
-  font-size: 16px;
-  color: ${({ theme }) => theme.textColor};
-  margin-bottom: 30px;
-`;
-
-const CurrentWeather = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-const Temperature = styled.div`
-  color: ${({ theme }) => theme.temperatureColor};
-  font-size: 96px;
-  font-weight: 300;
-  display: flex;
-`;
-
-const Celsius = styled.div`
-  font-weight: normal;
-  font-size: 42px;
-`;
-
-const AirFlow = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme }) => theme.textColor};
-  margin-bottom: 20px;
-
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Rain = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: ${({ theme }) => theme.textColor};
-
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
-`;
-
-const Refresh = styled.div`
-  position: absolute;
-  right: 15px;
-  bottom: 15px;
-  font-size: 12px;
-  display: inline-flex;
-  align-items: flex-end;
-  color: ${({ theme }) => theme.textColor};
-
-  svg {
-    margin-left: 10px;
-    width: 15px;
-    height: 15px;
-    cursor: pointer;
-    /* 使用 rotate 動畫效果在 svg 圖示上 */
-    animation: rotate infinite 1.5s linear;
-    /* 取得傳入的 props 並根據它來決定動畫要不要執行 */
-    animation-duration: ${({ isLoading }) => (isLoading ? "1.5s" : "0s")};
-  }
-
-  /* STEP 1：定義旋轉的動畫效果，並取名為 rotate */
-  @keyframes rotate {
-    from {
-      transform: rotate(360deg);
-    }
-    to {
-      transform: rotate(0deg);
-    }
-  }
-`;
 //fetch api 並回傳promise
 const fetchWeatherForecast = () => {
   return fetch(
@@ -247,17 +139,7 @@ const WeatherApp = () => {
     isLoading: true,
   });
 
-  const {
-    observationTime,
-    stationName,
-    temperature,
-    windSpeed,
-    description,
-    weatherCode,
-    rainPossibility,
-    comfortability,
-    isLoading,
-  } = weatherElement;
+  const { stationName } = weatherElement;
 
   //使用 useState 並定義 currentTheme 的預設值為 light
   const [currentTheme, setCurrentTheme] = useState("light");
@@ -309,41 +191,11 @@ const WeatherApp = () => {
     // 將當前選到的主題配色傳入 `theme` 中
     <ThemeProvider theme={theme[currentTheme]}>
       <Container>
-        {console.log(isLoading)}
-        {console.log("render")}
-        <WeatherCard>
-          <Location theme="dark">{stationName}</Location>
-          <Description>
-            {description} {comfortability}
-          </Description>
-          <CurrentWeather>
-            <Temperature>
-              {Math.round(temperature)} <Celsius>°C</Celsius>
-            </Temperature>
-            <WeatherIcon
-              currentWeatherCode={weatherCode}
-              moment={moment || "day"}
-            />
-          </CurrentWeather>
-          <AirFlow>
-            <AirFlowIcon />
-            {windSpeed} m/h
-          </AirFlow>
-          <Rain>
-            <RainIcon />
-            {rainPossibility} %
-          </Rain>
-          {/* 把 isLoading 的資料狀態透過 props 傳入 Styled Component */}
-          <Refresh onClick={fetchData} isLoading={isLoading}>
-            最後觀測時間：
-            {new Intl.DateTimeFormat("zh-TW", {
-              hour: "numeric",
-              minute: "numeric",
-            }).format(new Date(observationTime))}{" "}
-            {/* STEP 2：當 isLoading 的時候顯示 LoadingIcon 否則顯示 RefreshIcon */}
-            {isLoading ? <LoadingIcon /> : <RefreshIcon />}
-          </Refresh>
-        </WeatherCard>
+        <WeatherCard
+          weatherElement={weatherElement}
+          moment={moment}
+          fetchData={fetchData}
+        />
       </Container>
     </ThemeProvider>
   );
---
+++ src/WeatherCard.js
@@ -0,0 +1,169 @@
+import React from "react";
+import styled from "@emotion/styled";
+import WeatherIcon from "./WeatherIcon.js";
+
+//載入SVG圖片
+import { ReactComponent as CloudyIcon } from "./images/day-cloudy.svg";
+import { ReactComponent as AirFlowIcon } from "./images/airFlow.svg";
+import { ReactComponent as RainIcon } from "./images/rain.svg";
+import { ReactComponent as RefreshIcon } from "./images/refresh.svg";
+import { ReactComponent as LoadingIcon } from "./images/loading.svg";
+
+const WeatherCardWrapper = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
+
+// 透過 props 取得傳進來的資料
+// props 會是 {theme: "dark", children: "台北市"}
+// 透過傳進來的資料決定要呈現的樣式
+const Location = styled.div`
+  font-size: 28px;
+  color: ${({ theme }) => theme.titleColor};
+  margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+  font-size: 16px;
+  color: ${({ theme }) => theme.textColor};
+  margin-bottom: 30px;
+`;
+
+const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Temperature = styled.div`
+  color: ${({ theme }) => theme.temperatureColor};
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: ${({ theme }) => theme.textColor};
+  margin-bottom: 20px;
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: ${({ theme }) => theme.textColor};
+
+  svg {
+    width: 25px;
+    height: auto;
+    margin-right: 30px;
+  }
+`;
+
+const Refresh = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: flex-end;
+  color: ${({ theme }) => theme.textColor};
+
+  svg {
+    margin-left: 10px;
+    width: 15px;
+    height: 15px;
+    cursor: pointer;
+    /* 使用 rotate 動畫效果在 svg 圖示上 */
+    animation: rotate infinite 1.5s linear;
+    /* 取得傳入的 props 並根據它來決定動畫要不要執行 */
+    animation-duration: ${({ isLoading }) => (isLoading ? "1.5s" : "0s")};
+  }
+
+  /* STEP 1：定義旋轉的動畫效果，並取名為 rotate */
+  @keyframes rotate {
+    from {
+      transform: rotate(360deg);
+    }
+    to {
+      transform: rotate(0deg);
+    }
+  }
+`;
+//在參數中帶入 props 即可取得外層組件傳入的資料
+const WeatherCard = (props) => {
+  //透過物件的解構賦值從 props 中取出傳入的資料
+  const { weatherElement, moment, fetchData } = props;
+  // 將 weatherElement 中的資料透過解構賦值取出後，放置到 JSX 中使用
+  const {
+    observationTime,
+    locationName,
+    stationName,
+    temperature,
+    windSpeed,
+    description,
+    weatherCode,
+    rainPossibility,
+    comfortability,
+    isLoading,
+  } = weatherElement;
+
+  return (
+    <WeatherCardWrapper>
+      <Location theme="dark">{stationName}</Location>
+      <Description>
+        {description} {comfortability}
+      </Description>
+      <CurrentWeather>
+        <Temperature>
+          {Math.round(temperature)} <Celsius>°C</Celsius>
+        </Temperature>
+        <WeatherIcon
+          currentWeatherCode={weatherCode}
+          moment={moment || "day"}
+        />
+      </CurrentWeather>
+      <AirFlow>
+        <AirFlowIcon />
+        {windSpeed} m/h
+      </AirFlow>
+      <Rain>
+        <RainIcon />
+        {rainPossibility} %
+      </Rain>
+      {/* 把 isLoading 的資料狀態透過 props 傳入 Styled Component */}
+      <Refresh onClick={fetchData} isLoading={isLoading}>
+        最後觀測時間：
+        {new Intl.DateTimeFormat("zh-TW", {
+          hour: "numeric",
+          minute: "numeric",
+        }).format(new Date(observationTime))}{" "}
+        {/* STEP 2：當 isLoading 的時候顯示 LoadingIcon 否則顯示 RefreshIcon */}
+        {isLoading ? <LoadingIcon /> : <RefreshIcon />}
+      </Refresh>
+    </WeatherCardWrapper>
+  );
+};
+
+export default WeatherCard;
